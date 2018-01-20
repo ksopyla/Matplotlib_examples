17b453bb5c66a5ec4b75a6908f13088a4a16693b
ksopyla <sopyla@ermlab.com>
2018-01-20 16:40:20 +0100

    remove 3.5 Axes py it was empty

--- 3. Subplots and axes/3.6_Axes.py
+++ 3. Subplots and axes/3.6_Axes.py
@@ -5,7 +5,7 @@ import numpy as np
 pl.axes([.1, .1, .5, .5])
 pl.xticks(())
 pl.yticks(())
-pl.text(0.1, 0.1, 'axes([0.1,0.1,.8,.8])', ha='left', va='center',
+pl.text(0.1, 0.1, 'axes([0.1,0.1, .5, .5])', ha='left', va='center',
         size=16, alpha=.5)
 
 pl.axes([.2, .2, .5, .5])
